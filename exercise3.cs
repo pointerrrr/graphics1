9ff26a2a005189165a648ba06f8bd28b96ffd528
pointerrrr <gideonogilvie@gmail.com>
2017-04-26 00:03:45 +0200

    Exercise 3 completed

--- exercise3.cs
+++ exercise3.cs
@@ -7,21 +7,64 @@ namespace Template
 	class Exercise3 : Game
 	{
 		// member variables
-		
-		// initialize
+		float x1 = -1.0f, y1 = 1.0f;
+		float x2 = 1.0f, y2 = 1.0f;
+		float x3 = 1.0f, y3 = -1.0f;
+		float x4 = -1.0f, y4 = -1.0f;
 
+
+		// initialize
 		public override void Init()
 		{
 		}
-
+		float a = 0;
 		// tick: renders one frame
 		public override void Tick()
 		{
-			screen.Clear(0);
+			screen.Clear(0xffffff);
 			screen.Print("Exercise 3", 2, 2, 0xffffff);
-			screen.Line(2, 20, 160, 20, 0xff0000);
-			for (int i = 0; i < 256; i++)
-				screen.Line(screen.width / 2 + 127 - i, screen.height / 2 - 127, screen.width / 2 + 127 - i, screen.height / 2 + 127, CreateRGB(0, 0, 255 - i));
+			a = (float) ( Math.PI / 90 );
+			screen.Line(TX(0), TY(-2), TX(0), TY(2), 0x0);
+			screen.Line(TX(-2), TY(0), TX(2), TY(0), 0x0);
+			/*screen.Print("-1", 2, 2, 0xffffff);
+			screen.Print("-1", 2, 2, 0xffffff);
+			screen.Print("0", 2, 2, 0xffffff);
+			screen.Print("1", 2, 2, 0xffffff);
+			screen.Print("1", 2, 2, 0xffffff);
+			screen.Print("2", 2, 2, 0xffffff);
+			screen.Print("2", 2, 2, 0xffffff);*/
+			screen.Line(TX(rotateX(x1, y1)), TY(rotateY(x1, y1)), TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), 0xff0000);
+			screen.Line(TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), 0xff0000);
+			screen.Line(TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), TX(rotateX(x4, y4)), TY(rotateY(x4, y4)), 0xff0000);
+			screen.Line(TX(rotateX(x4, y4)), TY(rotateY(x4, y4)), TX(rotateX(x1, y1)), TY(rotateY(x1, y1)), 0xff0000);	
+		}
+
+
+		public int rotateX(float x, float y)
+		{
+			float rx = (float) ( x * Math.Cos(a) - y * Math.Sin(a) );			
+			return (int)rx;
+		}
+
+		public int rotateY(float x, float y)
+		{
+			float ry = (float) ( x * Math.Sin(a) + y * Math.Cos(a) );			
+			return (int)ry;
+		}
+
+		public int TX(float x)
+		{
+			x += 2;
+			x *= 160;
+			return (int)x;
+		}
+
+		public int TY(float y)
+		{
+			y += 2;
+			y *= 160;
+			y = 520 - y;
+			return (int)y;
 		}
 	}
 
