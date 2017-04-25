4d884828eb3e0d1c61eaecf6bf7f653415cf5b57
pointerrrr <gideonogilvie@gmail.com>
2017-04-26 00:15:54 +0200

    removed outcommented code in exercise 3 started exercise 4 (works for all resolutions now)

--- exercise3.cs
+++ exercise3.cs
@@ -21,18 +21,9 @@ namespace Template
 		// tick: renders one frame
 		public override void Tick()
 		{
-			screen.Clear(0xffffff);
+			screen.Clear(0);
 			screen.Print("Exercise 3", 2, 2, 0xffffff);
 			a = (float) ( Math.PI / 90 );
-			screen.Line(TX(0), TY(-2), TX(0), TY(2), 0x0);
-			screen.Line(TX(-2), TY(0), TX(2), TY(0), 0x0);
-			/*screen.Print("-1", 2, 2, 0xffffff);
-			screen.Print("-1", 2, 2, 0xffffff);
-			screen.Print("0", 2, 2, 0xffffff);
-			screen.Print("1", 2, 2, 0xffffff);
-			screen.Print("1", 2, 2, 0xffffff);
-			screen.Print("2", 2, 2, 0xffffff);
-			screen.Print("2", 2, 2, 0xffffff);*/
 			screen.Line(TX(rotateX(x1, y1)), TY(rotateY(x1, y1)), TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), 0xff0000);
 			screen.Line(TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), 0xff0000);
 			screen.Line(TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), TX(rotateX(x4, y4)), TY(rotateY(x4, y4)), 0xff0000);
