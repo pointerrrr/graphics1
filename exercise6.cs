a2c55b7889fef4f4a706162f0a96a97a8e215957
pointerrrr <gideonogilvie@gmail.com>
2017-05-01 01:20:38 +0200

    fixes for heightmap

--- exercise6.cs
+++ exercise6.cs
@@ -15,9 +15,9 @@ namespace Template
 		float scale = 8.0f;//width of screen
 		float origX = 0.0f, origY = 0.0f;
 		float a = 0.0f;
-		float b = 0.75f;
-		float c = -1.5f;
-
+		float b = 0.0f;
+		float c = 0.0f;
+		float d = 0.0f;
 		// initialize
 		public override void Init()
 		{
@@ -61,6 +61,7 @@ namespace Template
 			screen.Clear(0);
 			screen.Print("Exercise 6", 2, 2, 0xffffff);
 			screen.Print("A: " + a + " B: " + b + " C: " + c, 2, 30, 0xfffff);
+			//d += (float) Math.PI / 90;
 			/*screen.Line(TX(rotateX(x1, y1)), TY(rotateY(x1, y1)), TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), 0xff0000);
 			screen.Line(TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), 0xff0000);
 			screen.Line(TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), TX(rotateX(x4, y4)), TY(rotateY(x4, y4)), 0xff0000);
@@ -78,47 +79,40 @@ namespace Template
 			var M = Matrix4.CreatePerspectiveFieldOfView(1.6f, 1.3f, .1f, 1000);
 			GL.LoadMatrix(ref M);
 			GL.Translate(0, 0, -2);
+			GL.Rotate(110, 1, 0, 0);
+			GL.Rotate(d * 180 / Math.PI, 0, 0, 1);
+			//GL.Translate(0, 0, -2);
 			//GL.Rotate(20, -1, 0, 0);
 			GL.Rotate(a * 180 / Math.PI, 0, 0, 1);
 			GL.Rotate(b * 180 / Math.PI, 0, 1, 0);
 			GL.Rotate(c * 180 / Math.PI, 1, 0, 0);
-			/*for (int i = 0; i < 128; i++)
-				for (int j = 0; j < 128; j++)
-				{
-					GL.Color3(h[i,j], 0.0f, 0.0f);
-					GL.Begin(PrimitiveType.Triangles);
-					GL.Vertex3((float)i/127, (float)j/127, 0);
-					GL.Vertex3((float) i / 127 + 0.01f, (float) j / 127 + 0.01f, 0);
-					GL.Vertex3((float)i/127, (float)j/127, -h[i, j]);
-
-					GL.End();
-				}*/
-			/*GL.Color3(1.0f, 0.0f, 0.0f);
-			GL.Begin(PrimitiveType.Triangles);
-			GL.Vertex3(-1.0f, -1.0f, -4);
-			GL.Vertex3(1.0f, -1.0f, -4);
-			GL.Vertex3(0.0f, 1.0f, -4);
-			GL.End();*/
 			float depth = 0.5f;
 			float size = 0.01f;
 			float scale = 0.5f;
-			for (float i = 0; i < 127; i++)
-				for (float j = 0; j < 127; j++)
+			GL.Begin(PrimitiveType.Quads);
+			for (float i = 1; i < 126; i++)
+				for (float j = 1; j < 126; j++)
 				{
 					GL.Color3(h[(int) i, (int) j], 0.0f, 1.0f - h[(int) i, (int) j]);
-					GL.Begin(PrimitiveType.Quads);
+					
 					float f = size * 2;
 					float di = f * ( i - 63 );
 					float dj = f * ( j - 63 );
-					GL.Vertex3(-size + di, size + dj, ( h[(int) i, (int) j] - depth ) * scale);
-					GL.Vertex3(size + di, size + dj, -depth * scale);
-					GL.Vertex3(size + di, -size + dj, -depth * scale);
-					GL.Vertex3(-size + di, -size + dj, -depth * scale);
+					GL.Color3(1.0f, 0.0f, 0.0f);
+					GL.Vertex3(-size + di, size + dj, ( -h[(int) i-1, (int) j-1] - depth ) * scale);
 					
+					GL.Vertex3(size + di, size + dj, ( -h[(int) i, (int) j-1] - depth ) * scale);
+					GL.Color3(0.0f, 0.0f, 1.0f);
+					GL.Vertex3(size + di, -size + dj, ( -h[(int) i-1, (int) j] - depth ) * scale);
 					
-					
-					GL.End();
+					GL.Vertex3(-size + di, -size + dj, ( -h[(int) i, (int) j] - depth ) * scale);
+					GL.Color3(1.0f, 1.0f, 1.0f);
+
+
+
+
 				}
+			GL.End();
 		}
 
 		public float rotateX(float x, float y)
