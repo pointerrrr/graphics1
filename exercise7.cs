5a708680a6c337347c92c7464432f65e98cab9d8
pointerrrr <gideonogilvie@gmail.com>
2017-05-01 16:35:28 +0200

    exercise 7 + 8

--- exercise7.cs
+++ exercise7.cs
@@ -12,15 +12,63 @@ namespace Template
 	class Exercise7 : Game
 	{
 		// member variables
-		float scale = 8.0f;//width of screen
+		float[] vertexData;
+		float screenscale = 8.0f;//width of screen
 		float origX = 0.0f, origY = 0.0f;
 		float a = 0.0f;
-		float b = 0.75f;
-		float c = -1.5f;
+		float b = 0.0f;
+		float c = 0.0f;
+		float d = 0.0f;
+		float depth = 0.5f;
+		float size = 0.01f;
+		float scale = 0.5f;
+		int VBO;
+
+
+		Surface map;
+		float[,] h;
+
 
 		// initialize
 		public override void Init()
 		{
+			map = new Surface("../../assets/heightmap.png");
+			h = new float[128, 128];
+			for (int y = 0; y < 128; y++) for (int x = 0; x < 128; x++)
+					h[x, y] = ( (float) ( map.pixels[x + y * 128] & 255 ) ) / 256;
+			vertexData = new float[127 * 127 * 2 * 3 * 3];
+			int counter = 0;
+			for (int i = 0; i < 127; i++)
+				for (int j = 0; j < 127; j++)
+				{
+					float f = size * 2;
+					float di = f * ( i - 63 );
+					float dj = f * ( j - 63 );
+					//vertex 1
+					vertexData[counter++] = -size + di;
+					vertexData[counter++] = -size + dj;
+					vertexData[counter++] = ( -h[(int) i, (int) j] - depth ) * scale;
+					//vertex2
+					vertexData[counter++] = size + di;
+					vertexData[counter++] = -size + dj;
+					vertexData[counter++] = ( -h[(int) i + 1, (int) j] - depth ) * scale;
+					//vertex3
+					vertexData[counter++] = size + di;
+					vertexData[counter++] = size + dj;
+					vertexData[counter++] = ( -h[(int) i + 1, (int) j + 1] - depth ) * scale;
+					//vertex4
+					vertexData[counter++] = -size + di;
+					vertexData[counter++] = size + dj;
+					vertexData[counter++] = ( -h[(int) i, (int) j + 1] - depth ) * scale;
+					//vertex5
+					vertexData[counter++] = size + di;
+					vertexData[counter++] = size + dj;
+					vertexData[counter++] = ( -h[(int) i + 1, (int) j + 1] - depth ) * scale;
+					//vertex6
+					vertexData[counter++] = -size + di;
+					vertexData[counter++] = -size + dj;
+					vertexData[counter++] = ( -h[(int) i, (int) j] - depth ) * scale;
+				}
 		}
 
 		public override void Control(KeyboardState keys)
@@ -61,6 +109,7 @@ namespace Template
 			screen.Clear(0);
 			screen.Print("Exercise 7", 2, 2, 0xffffff);
 			screen.Print("A: " + a + " B: " + b + " C: " + c, 2, 30, 0xfffff);
+			//d += (float) Math.PI / 90;
 			/*screen.Line(TX(rotateX(x1, y1)), TY(rotateY(x1, y1)), TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), 0xff0000);
 			screen.Line(TX(rotateX(x2, y2)), TY(rotateY(x2, y2)), TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), 0xff0000);
 			screen.Line(TX(rotateX(x3, y3)), TY(rotateY(x3, y3)), TX(rotateX(x4, y4)), TY(rotateY(x4, y4)), 0xff0000);
@@ -69,55 +118,40 @@ namespace Template
 
 		public override void RenderGL()
 		{
-			Surface map;
-			float[,] h;			map = new Surface("../../assets/heightmap.png");
-			h = new float[128, 128];
-			for (int y = 0; y < 128; y++) for (int x = 0; x < 128; x++)
-					h[x, y] = ( (float) ( map.pixels[x + y * 128] & 255 ) ) / 256;
+			
 
 			var M = Matrix4.CreatePerspectiveFieldOfView(1.6f, 1.3f, .1f, 1000);
 			GL.LoadMatrix(ref M);
 			GL.Translate(0, 0, -2);
+			GL.Rotate(110, 1, 0, 0);
+			GL.Rotate(d * 180 / Math.PI, 0, 0, 1);
+
+			//GL.Translate(0, 0, -2);
 			//GL.Rotate(20, -1, 0, 0);
 			GL.Rotate(a * 180 / Math.PI, 0, 0, 1);
 			GL.Rotate(b * 180 / Math.PI, 0, 1, 0);
 			GL.Rotate(c * 180 / Math.PI, 1, 0, 0);
-			/*for (int i = 0; i < 128; i++)
-				for (int j = 0; j < 128; j++)
-				{
-					GL.Color3(h[i,j], 0.0f, 0.0f);
-					GL.Begin(PrimitiveType.Triangles);
-					GL.Vertex3((float)i/127, (float)j/127, 0);
-					GL.Vertex3((float) i / 127 + 0.01f, (float) j / 127 + 0.01f, 0);
-					GL.Vertex3((float)i/127, (float)j/127, -h[i, j]);
-
-					GL.End();
-				}*/
-			/*GL.Color3(1.0f, 0.0f, 0.0f);
-			GL.Begin(PrimitiveType.Triangles);
-			GL.Vertex3(-1.0f, -1.0f, -4);
-			GL.Vertex3(1.0f, -1.0f, -4);
-			GL.Vertex3(0.0f, 1.0f, -4);
-			GL.End();*/
-			float depth = 0.5f;
-			float size = 0.1f;
-			float scale = 0.1f;
-			float scaleh = 0.4f;
-			/*for (float i = 0; i < 127; i++)
+			
+			GL.Begin(PrimitiveType.Quads);
+			for (float i = 0; i < 127; i++)
 				for (float j = 0; j < 127; j++)
-				{*/
-			float i = 64, j = 64;
-			GL.Color3(h[(int) i, (int) j], 0.0f, 1.0f - h[(int) i, (int) j]);
-					GL.Begin(PrimitiveType.Quads);
+				{
 					float f = size * 2;
 					float di = f * ( i - 63 );
 					float dj = f * ( j - 63 );
-					GL.Vertex3(-size + di, -size + dj, -depth * scaleh);
-					GL.Vertex3(size + di, -size + dj, -depth * scaleh);
-					GL.Vertex3(-size + di, size + dj, ( h[(int) i, (int) j] - depth ) * scaleh);
-					GL.Vertex3(size + di, size + dj, -depth * scaleh);
-					GL.End();
-				//}
+					GL.Color3(h[(int) i, (int) j], 0.0f, 1.0f - h[(int) i, (int) j]);
+					GL.Vertex3(-size + di, -size + dj, ( -h[(int) i, (int) j] - depth ) * scale);
+
+					GL.Color3(h[(int) i + 1, (int) j], 0.0f, 1.0f - h[(int) i + 1, (int) j]);
+					GL.Vertex3(size + di, -size + dj, ( -h[(int) i + 1, (int) j] - depth ) * scale);
+
+					GL.Color3(h[(int) i + 1, (int) j + 1], 0.0f, 1.0f - h[(int) i + 1, (int) j + 1]);
+					GL.Vertex3(size + di, size + dj, ( -h[(int) i + 1, (int) j + 1] - depth ) * scale);
+
+					GL.Color3(h[(int) i, (int) j + 1], 0.0f, 1.0f - h[(int) i, (int) j + 1]);
+					GL.Vertex3(-size + di, size + dj, ( -h[(int) i, (int) j + 1] - depth ) * scale);
+				}
+			GL.End();
 		}
 
 		public float rotateX(float x, float y)
@@ -135,16 +169,16 @@ namespace Template
 		public int TX(float x)
 		{
 			x += origX;
-			x += scale / 2;
-			x *= screen.width / scale;
+			x += screenscale / 2;
+			x *= screen.width / screenscale;
 			return (int) x;
 		}
 
 		public int TY(float y)
 		{
 			y += origY;
-			y += scale / 2;
-			y *= screen.width / scale;
+			y += screenscale / 2;
+			y *= screen.width / screenscale;
 			y = screen.height + ( screen.width - screen.height ) / 2 - y;
 			return (int) y;
 		}
